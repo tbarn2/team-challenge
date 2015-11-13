72300cfc15a78a80a8424592c71acaac853b1a6b
Meera Limaye <mlimaye@uw.edu>
2015-11-13 14:03:15 -0800

    test files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-app.js
+'use strict';
+
+angular.module('SignUpApp', [])
+    .controller('FormCtrl', ['$scope', function($scope) {
+
+        $scope.submitForm = function(form){
+            if(form.$valid) {
+                alert("Sign Up info has been submitted!");
+            }
+            else {
+                alert("There was an error submitting your Sign Up info. Please check that your information is correct.");
+            }
+        }
+
+    }])
