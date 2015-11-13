544c983011b53b961b2e777cf729d5a4dd8b27bb
Meera Limaye <mlimaye@uw.edu>
2015-11-13 12:17:14 -0800

    test code

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -1,1 +1,22 @@
-protractor-conf.js
+describe('email', function () {
+	beforeEach(function() {
+		browser.get('http://localhost:8000');
+	})
+
+	it ('should check if email is valid', function() {		
+		browser.getEmail();
+
+		expect(getEmail).toEqual(email);
+		// If(getEmail == email)
+		// {
+		// 	test passes
+		// }
+		// Else
+		// {
+		// 	test fails
+		// }
+
+	});
+
+});
+
