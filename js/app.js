7b2fbcf69ba6cedbbd41fa51d40a3344f2c76069
Adrian Pacheco <adrianpacheco95@gmail.com>
2015-11-16 02:25:02 -0800

    test and spec updated

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('SignUpApp', [])
-    .controller('FormCtrl', ['$scope', function($scope) {
-
+    .controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
+        
+        var submit = false;
+        
         $scope.submitForm = function(form){
             if(form.$valid) {
                 alert("Sign Up info has been submitted!");
@@ -11,5 +13,34 @@ angular.module('SignUpApp', [])
                 alert("There was an error submitting your Sign Up info. Please check that your information is correct.");
             }
         }
-
-    }])
+        
+        $scope.reset = function () {
+            document.getElementById("success").innerHTML = "";
+            $scope.newForm.$setPristine();
+            $scope.newForm.$setUntouched(); 
+        }
+        
+        $scope.email = function() {
+            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($scope.email)) {
+        		$scope.signUpForm.email.$setValidity('email', true); 
+            } else {
+                $scope.signUpForm.email.$setValidity('email', false); 
+            }
+        }
+        
+        $scope.lastName = function () {
+            if ($scope.lastName.length >= 1) {
+                $scope.signUpForm.lastName.$setValidity('lastName', true);
+            } else {
+                $scope.signUpForm.lastName.$setValidity('lastName', false);
+            }
+        }
+        
+        $scope.password = function() { 
+            if($scope.password === $scope.passwordConfirm){
+                $scope.signUpForm.passwordConfirm.$setValidity('passwordConfirm', true); 
+            } else {
+                $scope.signUpForm.passwordConfrim.$setValidity('passwordConfrim', false); 
+            }
+        }
+    }]);
--- test/signup-spec.js
+++ test/signup-spec.js
@@ -11,26 +11,40 @@ describe('Sign Up Form', function() {
 
     expect(browser.getTitle()).toEqual('Sign Up Form');
     });
-
-    // last name test
-    it('should pass last name input', function() {
-        var input = element(by.model('name'));
-        input.sendKeys('Pacheco');
-        expect(element(by.id('lastNameShow')).isDisplayed()).toBe(false);
+    
+    it('should be invalid on a invalid email syntax', function() {
+        var email = element(by.id('email'));
+        email.sendKeys('informatics');
+        expect(email.getAttribute('class')).toMatch('ng-invalid');
+        expect(element(by.name('signUpForm')).getAttribute('class')).toMatch('ng-invalid');
+        expect(element(by.model('submit')).getAttribute('disabled')).toBe('true');
+    });
+    
+    it('should be valid on a valid email', function(){
+        var email = element(by.id('email'));
+        email.sendKeys('informatics@uw.edu');
+        expect(email.getAttribute('class')).toMatch('ng-valid');
+        expect(element(by.name('signUpForm')).getAttribute('class')).toMatch('ng-valid');
+    });
+    
+    it('should pass last name validation', function() {
+        var input = element(by.model('lastName'));
+        input.sendKeys('Ross');
+        expect((input, 'ng-invalid')).toEqual(true);
     });
 
-    // last name not beng passed
-    it('should not pass last name input', function() {
-        var input = element(by.model('name'));
-        input.click();
-        expect(element(by.id('lastNameShow'), 'ng-dirty')).toBe(true);
-
-        //var action = element(by.id('lastName'));
-
-        // if input field is touched, ng-show should be true
-
-
-        //expect(action.isEnabled()).toBe([true|false]);
-
+    it('should not pass last name validation', function() {
+        var input = element(by.model('lastName'));
+        input.sendKeys('Ross')
+        input.clear();
+        expect((input, 'ng-invalid').toEqual(false));
+    });
+    
+    it('should be valid on matching passwords', function(){
+        var password = element(by.id('password'));
+        var passwordConfirm = element(by.id('passwordConfirm'));
+        password.sendKeys('password1234');
+        passwordConfirm.sendKeys('password1234')
+        expect(element(by.id('passwordConfirm')).getAttribute('class')).toMatch('ng-valid');
     });
 });
